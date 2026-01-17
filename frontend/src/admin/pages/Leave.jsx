e718620cf050db1d4d50b9e9e4eecdfbf8d8a124
Ayush Pratap Singh <ayushdce2@gmail.com>
2026-01-17 12:22:17 +0530

    add

--- frontend/src/admin/pages/Leave.jsx
+++ frontend/src/admin/pages/Leave.jsx
@@ -18,21 +18,21 @@ const Leave = () => {
             <div className="bg-white dark:bg-gray-800 rounded-xl ">
 
 
-                <div className="  p-3 h-[calc(77vh-1rem)]  overflow-auto">
+                <div className="  p-3 h-[calc(77vh-1rem)] w-[21rem]  overflow-auto">
                     <h2 className="text-lg font-semibold mb-4 text-gray-700 dark:text-gray-200">
                         Leave History
                     </h2>
                     <div className='shadow'>
                         <div className='flex text-blue-400'>
-                            <div className='p-2 font-semibold w-16'>S.No.</div>
-                            <div className='p-2 font-semibold w-20'>Name</div>
-                            <div className='p-2 font-semibold w-35'>Email</div>
+                            <div className='p-2 font-semibold w-16 shrink-0'>S.No.</div>
+                            <div className='p-2 font-semibold w-20 shrink-0'>Name</div>
+                            <div className='p-2 font-semibold w-35 shrink-0'>Email</div>
 
-                            <div className='p-2 font-semibold w-25'>Leave Type</div>
-                            <div className='p-2 font-semibold w-25'>Total Days</div>
-                            <div className='p-2 font-semibold w-25'>From</div>
-                            <div className='p-2 font-semibold w-25'>To</div>
-                            <div className='p-2 font-semibold w-35'>Leave Status</div>
+                            <div className='p-2 font-semibold w-25 shrink-0'>Leave Type</div>
+                            <div className='p-2 font-semibold w-25 shrink-0'>Total Days</div>
+                            <div className='p-2 font-semibold w-25 shrink-0'>From</div>
+                            <div className='p-2 font-semibold w-25 shrink-0'>To</div>
+                            <div className='p-2 font-semibold w-35 shrink-0'>Leave Status</div>
 
                         </div>
 
@@ -43,15 +43,15 @@ const Leave = () => {
                                 return (
 
                                     <div className='flex text-blue-300 hover:bg-blue-50 dark:hover:bg-indigo-500' key={data._id}>
-                                        <div className='p-2  w-16 text-center'>{index + 1}</div>
-                                        <div className='p-2 w-20'>{data.name}</div>
-                                        <div className='p-2 w-35'>{data.email}</div>
+                                        <div className='p-2 w-16  shrink-0 text-center'>{index + 1}</div>
+                                        <div className='p-2 w-20  shrink-0'>{data.name}</div>
+                                        <div className='p-2 w-35  shrink-0'>{data.email}</div>
 
-                                        <div className='p-2 w-25 text-center'>{data.leave_type}</div>
-                                        <div className='p-2 w-25 text-green-300 text-center'>{data.total_leave_days}</div>
-                                        <div className='p-2 w-25'>{new Date(data.date_from).toLocaleDateString()}</div>
-                                        <div className='p-2 w-25'>{new Date(data.date_to).toLocaleDateString()}</div>
-                                        <div className='p-2 w-35 text-center text-green-500'>
+                                        <div className='p-2 w-25  shrink-0 text-center'>{data.leave_type}</div>
+                                        <div className='p-2 w-25  shrink-0 text-green-300 text-center'>{data.total_leave_days}</div>
+                                        <div className='p-2 w-25  shrink-0'>{new Date(data.date_from).toLocaleDateString()}</div>
+                                        <div className='p-2 w-25  shrink-0'>{new Date(data.date_to).toLocaleDateString()}</div>
+                                        <div className='p-2 w-35  shrink-0 text-center text-green-500'>
                                             <select onChange={(e) => { handleLeaveChanges(data._id, e.target.value) }} value={data.leave_status}>
                                                 <option value="waiting">Waiting</option>
                                                 <option value="approve">Approve</option>
